5d090dfd6582531d342ebeae0d0951d89d540b6d
triggxl <triggxl@hotmail.com>
2020-12-17 15:26:36 -0500

    first practice at deploying will be to put the Pokedex application into Heroku and making it available on the internet; completed steps to prepare for deployment

--- Pokedex-API/server.js
+++ Pokedex-API/server.js
@@ -1,27 +1,25 @@
-require('dotenv').config
+require('dotenv').config;
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json');
 const cors = require('cors');
 
 const app = express();
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting))
+app.use(helmet()); //https://github.com/helmetjs/helmet#how-it-works
 app.use(cors);
 
-console.log(process.env.API_TOKEN)
-
-//default array for GET/types
+//default GET/types
 const validTypes = ['Bugs', 'Dark', 'Dragon', 'Electric', `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
 
 app.use(function validateBearerToken(req, res, next) {
-  // const bearerToken = req.get('Authorization').split(' ')[1];
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization')
-  console.log('validate bearer token middleware');
   if(!authToken || authToken.split(' ')[1] !== apiToken) {
     res.status(401).json({error: 'Unathorized request'});
   }
-  //move on to next middleware
   next();
 })
 //separate middleware as cb into named function; passing into app method
@@ -36,21 +34,28 @@ const handleGetPokemon = (req, res) => {
 }
 
 app.get('/pokemon', handleGetPokemon), (req, res) => {
-  // const { name, type } = req.query.params;
-  // params.name = 
   let response = POKEDEX.pokemon;
-  //filter pokemon by name if name query param is present
+  //filter pokemon by name, if name query param is present
   if(req.query.name) {
     response = response.filter(pokemon => {pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())})
   }
-  //filter our pokemon by type if type query param is present
+  //filter pokemon by type, if type query param is present
   if(req.query.type) {
     response = response.filter(pokemon => {pokemon.type.toLowerCase().includes(req.query.name.toLowerCase())})
   }
   res.json(response)
 }
-
-const PORT = 8000;
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response;
+  if(process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
